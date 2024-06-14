32322ba33ec3cb592bd271434bc4447a84d100ba
srimannerella <srimannerella@outlook.com>
2024-06-14 11:21:48 +0530

    Add form validations and data transfer using intents.

--- app/src/main/java/com/example/login/Confirm.kt
+++ app/src/main/java/com/example/login/Confirm.kt
@@ -2,19 +2,28 @@ package com.example.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 
 class Confirm : AppCompatActivity() {
+
+    private lateinit var view1:TextView
+    private lateinit var view2:TextView
+    private lateinit var view3:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_confirm)
-//        val viewmodel = ViewModelProvider(this)[MainViewModel::class.java]
-        val v:MainViewModel by sharedView
-        val a = findViewById<LinearLayout>(R.id.details)
-        val mobile = a.findViewById<TextView>(R.id.view1)
-        mobile.text = viewmodel.mobile
+        view1 = findViewById(R.id.view1)
+        view2 = findViewById(R.id.view2)
+        view3 = findViewById(R.id.view3)
+        Log.d("Intent shit", "onCreate: $intent.")
+
+        view1.text = intent.getStringExtra("name")
+        view2.text = intent.getStringExtra("mobile")
+        view3.text = intent.getStringExtra("address")
+
 
 
     }
--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -3,47 +3,81 @@ package com.example.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewmodel.ViewModelFactoryDsl
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var viewmodek
+
+    private lateinit var mobile:EditText
+    private lateinit var name:EditText
+    private lateinit var address:EditText
+    private lateinit var passwd:EditText
+    private lateinit var rootLayout: ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val viewmodel = ViewModelProvider(this)[MainViewModel::class.java]
-        val mobile = findViewById<EditText>(R.id.mobile)
-        val name = findViewById<EditText>(R.id.name)
-        val address = findViewById<EditText>(R.id.address)
-        val passwd = findViewById<EditText>(R.id.passwd)
+        rootLayout = findViewById<ConstraintLayout>(R.id.main)
+        mobile = findViewById(R.id.mobile)
+        name = findViewById(R.id.name)
+        address = findViewById(R.id.address)
+        passwd = findViewById(R.id.passwd)
         val button = findViewById<Button>(R.id.login_button)
         button.setOnClickListener {
-            val intent = Intent(this, Confirm::class.java)
-            startActivity(intent)
+            if(validateRules()){
+                val intent = Intent(this, Confirm::class.java)
+                intent.putExtra("mobile", mobile.text.toString())
+                intent.putExtra("name", name.text.toString())
+                intent.putExtra("address", address.text.toString())
+                intent.putExtra("passwd", passwd.text.toString())
+                startActivity(intent)
+                finish()
+            }
         }
-        mobile.addTextChangedListener(object:TextWatcher{
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                val inputText = s.toString()
-                viewmodel.mobile = inputText
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                val inputText = s.toString()
-                if(inputText.length < 10){
-                    Toast.makeText(this@MainActivity, "Enter mobile number correctly",Toast.LENGTH_SHORT).show()
-                }
-            }
-
-        })
     }
+
+
+    fun validateRules(): Boolean {
+        var isValid = true
+
+        if (mobile.text.isEmpty()) {
+            mobile.error = "Mobile number should not be empty"
+            Snackbar.make(rootLayout, "Mobile number should not be empty", Snackbar.LENGTH_SHORT).show()
+            isValid = false
+        } else if (mobile.text.length < 10) {
+            mobile.error = "Mobile number should be 10 digits"
+            Snackbar.make(rootLayout, "Mobile number should be 10 digits", Snackbar.LENGTH_SHORT).show()
+            isValid = false
+        }
+
+        if (name.text.isEmpty()) {
+            name.error = "Name should not be empty"
+
+            Snackbar.make(rootLayout, "Name should not be empty", Snackbar.LENGTH_SHORT).show()
+            isValid = false
+        }
+
+        if (address.text.isEmpty()) {
+            address.error = "Address should not be empty"
+            Snackbar.make(rootLayout, "Address should not be empty", Snackbar.LENGTH_SHORT).show()
+            isValid = false
+        }
+
+        if (passwd.text.isEmpty()) {
+            passwd.error = "Password should not be empty"
+            Snackbar.make(rootLayout, "Password should not be empty", Snackbar.LENGTH_SHORT).show()
+            isValid = false
+        } else if (passwd.text.length < 8) {
+            passwd.error = "Password should be greater than 8 characters"
+            Snackbar.make(rootLayout, "Password should be greater than 8 characters", Snackbar.LENGTH_SHORT).show()
+            isValid = false
+        }
+
+        return isValid
+    }
+
+
 }
